5d718efe3ca53ddcc64bbf7c0e00e110aebb332d
cliffordaustin <cliffordaustin670@gmail.com>
2024-11-10 20:10:26 +0000

    Update

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = ["django-env.eba-psp8wtxz.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["Cnn-idc-detector-env.eba-cypachkg.us-west-2.elasticbeanstalk.com"]
 
 RENDER_EXTERNAL_HOSTNAME = env.str("RENDER_EXTERNAL_HOSTNAME", default=None)
 if RENDER_EXTERNAL_HOSTNAME:
