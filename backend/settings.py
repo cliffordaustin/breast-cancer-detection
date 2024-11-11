da346fa3447824e993c060c7a2d4bbf7d11241c3
cliffordaustin <cliffordaustin670@gmail.com>
2024-11-11 12:04:15 +0000

    Update

--- backend/settings.py
+++ backend/settings.py
@@ -21,10 +21,6 @@ env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
